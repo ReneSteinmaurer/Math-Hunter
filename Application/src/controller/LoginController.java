7f19029f2abea1342b3124299d06a33185f02d82
ReneSteinmaurer <55176260+ReneSteinmaurer@users.noreply.github.com>
2021-04-08 16:01:29 +0200

    login update

---
+++ Application/src/controller/LoginController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML private TextField inputName;
+    @FXML private Label labelName;
+    @FXML private Label labelPasw;
+    @FXML private TextField inputPasw;
+    @FXML private Button buttonLogin;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
